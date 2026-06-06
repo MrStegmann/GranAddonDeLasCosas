6d308a5b694aff7ba365d2367a8c4b9dac1776d1
MrStegmann <pjstegmann@outlook.es>
2026-06-07 00:39:58 +0200

    Fix: BroadCastRollMessage that never arrieved to the receiver. Locales function refactor.

--- src/Locales/index.lua
+++ src/Locales/index.lua
@@ -4,5 +4,5 @@ function GAC:_(key)
     if GAC.Locales and GAC.Locales.ES_es and GAC.Locales.ES_es[key] then
         return GAC.Locales.ES_es[key]
     end
-    return string.format("Translate for %s doesn't existe", tostring(key))
+    return key
 end
